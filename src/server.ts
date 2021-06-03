14224cbebc85752c94dc4f20c86e9e79af79bc55
pedrobcavalcante <54650692+pedrobcavalcante@users.noreply.github.com>
2021-06-03 19:22:27 -0300

    adicionado express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,8 @@
-console.log("hello world!");
+import express from 'express';
+// rest of the code remains same
+const app = express();
+const PORT = 8000;
+app.get('/', (req, res) => res.send('Server is Working!'));
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+});
